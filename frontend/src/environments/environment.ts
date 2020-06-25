780476b78882452e74bf4588c20d900646e72e91
Halima Josh <studentimamu@gmail.com>
2020-06-25 15:19:38 -0700

    updating domain&audience
    updating the domain and audiance with new values in Auth0

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    url: 'fsndmh.us', // the auth0 domain prefix
+    audience: 'HTTP://127.0.0.1:8080', // the audience set for the auth0 app
+    clientId: '7m0MY2c0UJmQRFDQPVgDtHUsasOQwJ1u', // the client id generated for the auth0 app
+    callbackURL: 'http://127.0.0.1:8100', // the base url of the running ionic application. 
   }
 };
